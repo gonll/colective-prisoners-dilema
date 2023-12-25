b05d7c3a81204932b528e3ec2b78b799ca7ca5fb
Gonzalo Herrera <99346834+gonzahll@users.noreply.github.com>
2023-12-25 14:06:49 -0300

    Git ignore settings

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 /// SETTINGS
 export const numberOfRandomPrisoners = 500; // How many random prisooners do we wanna use?
-export const minRounds = 1; //Prisoners should compete at least this ammount of times with each other.
-export const maxRounds = 2; //Prisoners should compete at max this ammount of times with each other.
+export const minRounds = 100; //Prisoners should compete at least this ammount of times with each other.
+export const maxRounds = 250; //Prisoners should compete at max this ammount of times with each other.
 export const showAmmountResults = 200; // How many prisoners should be listed in podium
 
 export const ammountOfGames = 1; // How many times the game should run.
