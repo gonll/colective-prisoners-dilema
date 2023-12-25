cc3819a76e4995640a3150537b437264851d2c56
Gonzalo Herrera <99346834+gonzahll@users.noreply.github.com>
2023-12-25 11:20:23 -0300

    Refactor settings to a separate file

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,11 @@ import { exit } from 'process';
 import {type Prisoner, Prisoners} from './prisoners';
 import { strategies } from './strats';
 import * as fs from 'fs';
-
+import {numberOfRandomPrisoners, minRounds, maxRounds, showAmmountResults, ammountOfGames, runAllStrats} from './settings'
 export type Decision = 'cooperate' | 'defect';
 
 const {prisoners} = Prisoners.Instance; 
 if(!prisoners.length)exit();
-
-/// SETTINGS
-const numberOfRandomPrisoners = 500; // How many random prisooners do we wanna use?
-const minRounds = 100; //Prisoners should compete at least this ammount of times with each other.
-const maxRounds = 200; //Prisoners should compete at max this ammount of times with each other.
-const ammountOfGames = 1; // How many times the game should run.
-const showAmmountResults = 20; // How many prisoners should be listed in podium
-
-const runAllStrats = false; // Runs all available strats against each other. Won't create random nor repeated prisoners.
 
 function simulateGame(prisonerA: Prisoner, prisonerB: Prisoner, rounds: number, indexA: number, indexB: number): [number, number] {
     let historyA: Decision[] = [];
@@ -62,8 +53,9 @@ function updateScores(decisionA: Decision, decisionB: Decision, scoreA: number, 
     return [scoreA, scoreB];
 }
 
+// Precompute keys
+const strategyKeys = Object.keys(strategies);
 function createRandomPrisoner(id: number): Prisoner {
-    const strategyKeys = Object.keys(strategies);
     const randomStrategyKey = strategyKeys[Math.floor(Math.random() * strategyKeys.length)];
     const randomStrategy = strategies[randomStrategyKey];
     const errorMargin = parseFloat((Math.random() * 0.5).toFixed(1)); // Error margin between 0 and 0.2
@@ -84,42 +76,39 @@ for (let i = 0; i < numberOfRandomPrisoners; i++) {
     prisoners.push(randomPrisoner);
 }
 
-const run = () => {
-    if(runAllStrats){
-        console.log('Run all available strategies against each other enabled. No random nor repeated prisoners will be created.')
-        const stratPrisoner = [];
-        for(const strat in strategies){
-            stratPrisoner.push({
-                name: strat,
-                errorMargin: 0,
-                finalScore: 0,
-                numberOfOpponents: 0,
-                description: '',
-                realStrategy: strategies[strat],
-                publicStrategy: strategies[strat],
-            })
-        }
-        // Run game for all strats
-        for (let indexA = 0; indexA < stratPrisoner.length; indexA++) {
-            for (let indexB = indexA + 1; indexB < stratPrisoner.length; indexB++) {
-                let rounds = Math.floor(Math.random() * maxRounds) + minRounds;
-                simulateGame(stratPrisoner[indexA], stratPrisoner[indexB], rounds, indexA, indexB);
-            }
-        }
-    }else{
-        // Run game
-        for (let indexA = 0; indexA < prisoners.length; indexA++) {
-            for (let indexB = indexA + 1; indexB < prisoners.length; indexB++) {
-                let rounds = Math.floor(Math.random() * maxRounds) + minRounds;
-                simulateGame(prisoners[indexA], prisoners[indexB], rounds, indexA, indexB);
-            }
+const run = (computedPrisoners: Prisoner[]) => {
+    // Run game
+    for (let indexA = 0; indexA < computedPrisoners.length; indexA++) {
+        for (let indexB = indexA + 1; indexB < computedPrisoners.length; indexB++) {
+            let rounds = Math.floor(Math.random() * maxRounds) + minRounds;
+            simulateGame(computedPrisoners[indexA], computedPrisoners[indexB], rounds, indexA, indexB);
         }
     }
 }
+const runAllGames = (computedPrisoners: Prisoner[]) => {
+    for(let i = 1; i <= ammountOfGames; i++ ){
+        console.log("🚀 ~ Run number: ", i)
+        run(computedPrisoners)
+    }
+}
 
-for(let i = 1; i <= ammountOfGames; i++ ){
-    console.log("🚀 ~ Run number: ", i)
-    run()
+if(runAllStrats){
+    console.log('Run all available strategies against each other enabled. No random nor repeated prisoners will be created.')
+    const stratPrisoner = [];
+    for(const strat in strategies){
+        stratPrisoner.push({
+            name: strat,
+            errorMargin: 0,
+            finalScore: 0,
+            numberOfOpponents: 0,
+            description: '',
+            realStrategy: strategies[strat],
+            publicStrategy: strategies[strat],
+        })
+    }
+    runAllGames(stratPrisoner);
+}else{
+    runAllGames(prisoners);
 }
 
 console.log('--------------------------------------------');
@@ -127,12 +116,12 @@ console.log('End result: ');
 console.log('--------------------------------------------');
 // Sort prisoners based on their average score per round
 prisoners.sort((a, b) => {
-    const averageScoreA = a.finalScore / a.numberOfOpponents;
-    const averageScoreB = b.finalScore / b.numberOfOpponents;
+    const averageScoreA = a.finalScore / (a.numberOfOpponents || 1);
+    const averageScoreB = b.finalScore / (b.numberOfOpponents || 1);
     return averageScoreB - averageScoreA;
 });
 // Write out results
-fs.writeFile(`./results/results-${new Date().toLocaleDateString().replaceAll('/','-')}.txt`, JSON.stringify(prisoners, null, 2), function(err) {
+fs.writeFile(`./results/results-${new Date().toLocaleDateString().replaceAll('/','-')}-${Date.now()}.txt`, JSON.stringify(prisoners, null, 2), function(err) {
     if(err) {
         return console.log(err);
     }
---
+++ src/settings.ts
@@ -0,0 +1,8 @@
+/// SETTINGS
+export const numberOfRandomPrisoners = 500; // How many random prisooners do we wanna use?
+export const minRounds = 100; //Prisoners should compete at least this ammount of times with each other.
+export const maxRounds = 200; //Prisoners should compete at max this ammount of times with each other.
+export const showAmmountResults = 20; // How many prisoners should be listed in podium
+
+export const ammountOfGames = 5; // How many times the game should run.
+export const runAllStrats = true; // Runs all available strats against each other. Won't create random nor repeated prisoners.
