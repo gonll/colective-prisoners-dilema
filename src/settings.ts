e7b59610d075b47bcaf701bd8df58a540ca1816f
Gonzalo Herrera <99346834+gonzahll@users.noreply.github.com>
2023-12-25 14:13:35 -0300

    Set default configs

--- src/settings.ts
+++ src/settings.ts
@@ -1,8 +1,8 @@
 /// SETTINGS
-export const numberOfRandomPrisoners = 500; // How many random prisooners do we wanna use?
-export const minRounds = 100; //Prisoners should compete at least this ammount of times with each other.
-export const maxRounds = 250; //Prisoners should compete at max this ammount of times with each other.
-export const showAmmountResults = 200; // How many prisoners should be listed in podium
+export const numberOfRandomPrisoners = 100; // How many random prisooners do we wanna use?
+export const minRounds = 150; //Prisoners should compete at least this ammount of times with each other.
+export const maxRounds = 300; //Prisoners should compete at max this ammount of times with each other.
+export const showAmmountResults = 20; // How many prisoners should be listed in podium
 
-export const ammountOfGames = 1; // How many times the game should run.
+export const ammountOfGames = 5; // How many times the game should run.
 export const runAllStrats = true; // Runs all available strats against each other. Won't create random nor repeated prisoners.
